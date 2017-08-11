e0856c9e31aabc21f3b569d6a89c281a1d786f0c
Reza <reza.irvanda@xtremax.com>
2017-08-11 17:10:07 +0700

    Add ImagePopulator for retrieve all images

---
+++ ImageResizer.Business.Test/UnitTest/ImagePopulatorTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ImageResizer.Business.Populator;
+
+namespace ImageResizer.Business.Test.UnitTest
+{
+    [TestClass]
+    public class ImagePopulatorTest
+    {
+        public string FileTestPathFormat = @"D:\My Toy\C#\ImageResizer\ImageResizer.Business.Test\UnitTest\TestFiles\{0}";
+        public ImagePopulator populator;
+
+        public ImagePopulatorTest()
+        {
+            populator = new ImagePopulator(string.Format(FileTestPathFormat,""));
+        }
+
+        [TestMethod]
+        public void TestPopulateFileList()
+        {
+            var fileList = populator.FilePathList;
+
+            Assert.AreNotEqual(fileList.Count, 0);
+        }
+    }
+}
---
+++ ImageResizer.Business/Populator/ImagePopulator.cs
@@ -0,0 +1,55 @@
+﻿using ImageResizer.Business.Util;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageResizer.Business.Populator
+{
+    public class ImagePopulator
+    {
+        private List<string> _ImageExtensions;
+        private string _FolderPath;
+        private List<string> _FilePathList;
+
+        public ImagePopulator(string folderPath)
+        {
+            _FolderPath = folderPath;
+            _FilePathList = new List<string>();
+            _ImageExtensions = ConfigUtil.GetAllowedExtensions();
+        }
+
+        public List<string> FilePathList
+        {
+            get
+            {
+                _Populate();
+                return _FilePathList;
+            }
+        }
+
+        private void _Populate()
+        {
+            if (_FilePathList.Count == 0)
+            {
+                _FilePathList.AddRange(_GetFilesFromDirectory(_FolderPath));
+            }
+        }
+
+        private List<string> _GetFilesFromDirectory(string folderPath)
+        {
+            var files = new List<string>();
+            var currentFolder = new DirectoryInfo(folderPath);
+
+            foreach (var folder in currentFolder.GetDirectories())
+            {
+                files.AddRange(_GetFilesFromDirectory(folder.FullName));
+            }
+
+            files.AddRange(currentFolder.GetFiles().Where(x => _ImageExtensions.Contains(x.Extension.ToLower())).Select(x => x.FullName));
+            return files;
+        }
+    }
+}
