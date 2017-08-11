2028e86fccd7c20f6c4a42bfc56f411034f3f939
Reza <reza.irvanda@xtremax.com>
2017-08-11 11:51:09 +0700

    Create abstract class as image resizer parent, to ease testing

---
+++ ImageResizer.Business/Resizer/Class/AbstractImageResizer.cs
@@ -0,0 +1,36 @@
+﻿using ImageResizer.Business.Resizer.Interface;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Text;
+
+namespace ImageResizer.Business.Resizer.Class
+{
+    public abstract class AbstractImageResizer : IResizer
+    {
+        public abstract bool Resize(string filePath, int quality = 100);
+
+        public ImageCodecInfo GetImageCodec(Bitmap bmp)
+        {
+            foreach (var codec in ImageCodecInfo.GetImageEncoders())
+            {
+                if (codec.FormatID.Equals(bmp.RawFormat.Guid))
+                {
+                    return codec;
+                }
+            }
+
+            return null;
+        }
+
+        public string GetNewFileName(string oldFileName, int quality)
+        {
+            var newFileNameBuilder = new StringBuilder(); ;
+            var lastBackSlash = oldFileName.LastIndexOf('\\');
+            if (lastBackSlash != -1)
+            {
+                newFileNameBuilder.AppendFormat("{0}\\{1}_compressed_{2}", oldFileName.Substring(0, lastBackSlash + 1), quality, oldFileName.Substring(lastBackSlash + 1)); //lastBackSlash should not be last character
+            }
+            return newFileNameBuilder.ToString();
+        }
+    }
+}
--- ImageResizer.Business/Resizer/Class/ImageResizer.cs
+++ ImageResizer.Business/Resizer/Class/ImageResizer.cs
@@ -8,13 +8,13 @@ using System.Drawing.Imaging;
 
 namespace ImageResizer.Business.Resizer.Class
 {
-    public class ImageResizer : IResizer
+    public class ImageResizer : AbstractImageResizer
     {
-        bool IResizer.Resize(string filePath, int quality)
+        public override bool Resize(string filePath, int quality)
         {
             using (var bmp = new Bitmap(filePath))
             {
-                var imageCodec = _GetImageCodec(bmp);
+                var imageCodec = GetImageCodec(bmp);
                 if (imageCodec == null)
                 {
                     return false;
@@ -24,34 +24,12 @@ namespace ImageResizer.Business.Resizer.Class
                 var encoderParameters = new EncoderParameters(1); // Only gonna modify quality parameters ("qualityEncoderParameter")
                 var qualityEncoderParameter = new EncoderParameter(qualityEncoder, quality);
                 encoderParameters.Param[0] = qualityEncoderParameter;
-                bmp.Save(_GetNewFileName(filePath,quality),imageCodec, encoderParameters);
+                bmp.Save(GetNewFileName(filePath,quality),imageCodec, encoderParameters);
 
                 return true;
             };
         }
 
-        private ImageCodecInfo _GetImageCodec(Bitmap bmp)
-        {
-            foreach(var codec in ImageCodecInfo.GetImageEncoders())
-            {
-                if (codec.FormatID.Equals(bmp.RawFormat.Guid))
-                {
-                    return codec;
-                }
-            }
-
-            return null;
-        }
-
-        private string _GetNewFileName(string oldFileName, int quality)
-        {
-            var newFileNameBuilder = new System.Text.StringBuilder(); ;
-            var lastBackSlash = oldFileName.LastIndexOf('\\');
-            if ( lastBackSlash != -1)
-            {
-                newFileNameBuilder.AppendFormat("{0}\\{1}_compressed_{2}",oldFileName.Substring(0, lastBackSlash+1),quality,oldFileName.Substring(lastBackSlash+1)); //lastBackSlash should not be last character
-            }            
-            return newFileNameBuilder.ToString();
-        }
+        
     }
 }
