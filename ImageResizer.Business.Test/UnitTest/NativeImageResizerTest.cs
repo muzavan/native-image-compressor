b501596a5ffe5ae894c3de29ace16156bc09a442
Reza <reza.irvanda@xtremax.com>
2017-08-11 15:05:37 +0700

    Revise Resize Test

--- ImageResizer.Business.Test/UnitTest/NativeImageResizerTest.cs
+++ ImageResizer.Business.Test/UnitTest/NativeImageResizerTest.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Imaging;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,8 +25,21 @@ namespace ImageResizer.Business.Test.UnitTest
         [TestMethod]
         public void TestResize()
         {
-            Resizer.Resize(string.Format(FileTestPathFormat, "Blue Sky.jpg"), 0);
-            Resizer.Resize(string.Format(FileTestPathFormat, "Marbles.PNG"), 30);
+            var newBlue = Resizer.Resize(string.Format(FileTestPathFormat, "Blue Sky.jpg"), 0);
+            var newMarble = Resizer.Resize(string.Format(FileTestPathFormat, "Marbles.PNG"), 30);
+
+            Assert.IsTrue(_IsSuccessfulResize(newBlue, string.Format(FileTestPathFormat, "Blue Sky.jpg")));
+            Assert.IsTrue(_IsSuccessfulResize(newMarble, string.Format(FileTestPathFormat, "Marbles.PNG")));
+        }
+
+
+        private bool _IsSuccessfulResize(string newFile, string oldFile)
+        {
+            //Check the success of resizing by comparing their size
+            var newFileInfo = new FileInfo(newFile);
+            var oldFileInfo = new FileInfo(oldFile);
+
+            return newFileInfo.Length <= oldFileInfo.Length;
         }
     }
 }
