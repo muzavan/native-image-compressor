fbd1c2c8c627fa0343cf108aaac7382f019979d4
Reza <reza.irvanda@xtremax.com>
2017-08-12 16:45:02 +0700

    Add ImageCompressionUtil

---
+++ ImageCompressor.Business.Test/UnitTest/ImageCompressionUtilTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ImageCompressor.Business.Compressor.Class;
+using System.Diagnostics;
+
+namespace ImageCompressor.Business.Test.UnitTest
+{
+    [TestClass]
+    public class ImageCompressionUtilTest
+    {
+        private Util.ImageCompressionUtil<NativeImageCompressor> _Util;
+
+        public ImageCompressionUtilTest()
+        {
+            _Util = new Util.ImageCompressionUtil<NativeImageCompressor>(50);
+        }
+        [TestMethod]
+        public void TestCompress()
+        {
+            var stopwatch = Stopwatch.StartNew();
+            _Util.Compress();
+            var time = stopwatch.ElapsedMilliseconds;
+        }
+    }
+}
---
+++ ImageCompressor.Business/Util/ImageCompressionUtil.cs
@@ -0,0 +1,35 @@
+﻿using ImageCompressor.Business.Compressor.Class;
+using ImageCompressor.Business.Populator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageCompressor.Business.Util
+{
+    public class ImageCompressionUtil<T> where T : AbstractImageCompressor, new()
+    {
+        private ImagePopulator _Populator;
+        private AbstractImageCompressor _Compressor;
+        private int _Quality;
+
+        public ImageCompressionUtil(int quality) : this(ConfigUtil.GetFolderPath(), quality)
+        {
+            // do nothing
+        }
+
+        public ImageCompressionUtil(string folderPath,  int quality)
+        {
+            _Populator = new ImagePopulator(folderPath);
+            _Compressor = new T();
+            _Quality =  quality;
+        }
+
+        public void Compress()
+        {
+            var filePaths = _Populator.FilePathList;
+            _Compressor.BatchResize(filePaths, _Quality);
+        }
+    }
+}
